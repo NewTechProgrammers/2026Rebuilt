ec1e77c3a1171cd7427f8be90e331a55442301b9
francistudor <uuidpro@proton.me>
2026-02-17 16:27:40 +0100

    Fix Shooting constants percent outputs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -41,13 +41,15 @@ public final class Constants {
   }
 
   public static final class ShootingConstants {
+    // We will need to change these later
     public static final int kSortingSparkMaxPort = 77;
     public static final int kPassthroughSparkMaxPort = 78;
     public static final int kShooterSparkMaxPort = 79;
-
-    public static final int kPercentOutputSorting = 77;
-    public static final int kPercentOutputPassthrough = 78;
-    public static final int kPercentOutputShooter = 79;
+    
+    // We will need to change these later
+    public static final double kPercentOutputSorting = 0.30;
+    public static final double kPercentOutputPassthrough = 0.30;
+    public static final double kPercentOutputShooter = 0.30;
   }
 
   public static final class AutoConstants {
